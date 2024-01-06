4f36b007d75bd2dc5c1713a7b635fbe740ccaef0
GrCOTE7 <grcote7@gmail.com>
2024-01-06 04:30:24 +0100

    Feat: CSV saving

--- basis.js
+++ basis.js
@@ -2,8 +2,8 @@
 // import { basis } from './01_basis/01_screenshot.js';
 // basis();
 
-import { amazon } from './01_basis/04_allPdts_allPages.js';
+// import { amazon } from './01_basis/04_allPdts_allPages.js';
 // import { amazon } from './01_basis/t.js';
 // import { amazon } from './ia.js';
 
-// import { test } from './01_basis/t.js';
+import { test } from './01_basis/t.js';
