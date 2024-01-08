8af0da23a1be2caabd01c47a25d1ccb2678aa79b
GrCOTE7 <grcote7@gmail.com>
2024-01-08 15:46:21 +0100

    Feat: All paremeters for Chrome usage

--- 01_basis/classes/testIfRobot.js
+++ 01_basis/classes/testIfRobot.js
@@ -5,32 +5,34 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 export function testIfRobot(filename) {
- const test = () => {
-  const filePath = path.resolve(fileURLToPath(import.meta.url), '../../imgs/' + filename + '.png');
-  console.log(filePath);
+   const test = () => {
+      const filePath = path.resolve(fileURLToPath(import.meta.url), '../../imgs/' + filename + '.png');
+      console.log(filePath);
 
-  (async () => {
+      (async () => {
 
-   puppeteer.use(StealthPlugin());
+         puppeteer.use(StealthPlugin());
 
-   const browser = await puppeteer.launch({
-    headless: false,
-    // slowMo: 200,
-    // defaultViewport: false,
-    // userDataDir: './tmp',
-    // devtools: true
-    executablePath:"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-   });
-   console.log('Running test...');
-   const page = await browser.newPage();
-   await page.goto('https://bot.sannysoft.com', { waitUntil: 'networkidle2' });
-   await new Promise(r => setTimeout(r, 5000));
-   await page.screenshot({ path: filePath, fullPage: true });
+         const browser = await puppeteer.launch({
+            headless: false,
+            // slowMo: 200,
+            // defaultViewport: false,
+            // userDataDir: './tmp',
+            // devtools: true
+            // See chrome parameters in chrome'url: chrome://version
+            executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe", 
+            userDataDir: "C:/Users/utilisateur/AppData/Local/Google/Chrome/User Data/Default"
+         });
+         console.log('Running test...');
+         const page = await browser.newPage();
+         await page.goto('https://bot.sannysoft.com', { waitUntil: 'networkidle2' });
+         await new Promise(r => setTimeout(r, 5000));
+         await page.screenshot({ path: filePath, fullPage: true });
 
-   console.log('Screenshot done.')
-   await browser.close();
-  })()
+         console.log('Screenshot done.')
+         await browser.close();
+      })()
 
- }
- test();
+   }
+   test();
 }
